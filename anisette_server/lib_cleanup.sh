d0c52a1d7e8b237916d46e4ff658d8513f1ce3a9
Macleykun <wesley.de.vree@gmail.com>
2022-10-27 20:52:53 +0200

    Add cleanup script to remove unneeded libs

---
+++ anisette_server/lib_cleanup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [ "$(uname -m)" = 'aarch64' ]; then
+  mv lib/arm64-v8a .
+  rm -r applemusic.apk lib/*
+  mv arm64-v8a lib/
+elif [[ "$(uname -m)" = 'arm'* ]]; then
+  mv lib/armeabi-v7a .
+  rm -r applemusic.apk lib/*
+  mv armeabi-v7a lib/
+elif [ "$(uname -m)" = 'x86_64' ]; then
+  mv lib/x86_64 .
+  rm -r applemusic.apk lib/*
+  mv x86_64 lib/
+elif [ "$(uname -m)" = 'i686' ]; then
+  mv lib/x86 .
+  rm -r applemusic.apk lib/*
+  mv x86 lib/
+fi
