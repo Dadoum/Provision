f0dfe6332410e7b8d92ce736ccaea3d6c6c68f4f
Macley <26381427+Macleykun@users.noreply.github.com>
2022-10-29 23:22:19 +0200

    Instead of moving, unzip immediatly the right subfolder

--- anisette_server/docker-entrypoint.sh
+++ anisette_server/docker-entrypoint.sh
@@ -1,24 +1,15 @@
 #!/bin/bash
 
 curl https://apps.mzstatic.com/content/android-apple-music-apk/applemusic.apk -O
-unzip applemusic.apk "lib/*"
 
 if [ "$(uname -m)" = 'aarch64' ]; then
-  mv lib/arm64-v8a .
-  rm -r applemusic.apk lib/*
-  mv arm64-v8a lib/
+  unzip applemusic.apk "lib/arm64-v8a/*"
 elif [[ "$(uname -m)" = 'arm'* ]]; then
-  mv lib/armeabi-v7a .
-  rm -r applemusic.apk lib/*
-  mv armeabi-v7a lib/
+  unzip applemusic.apk "lib/armeabi-v7a/*"
 elif [ "$(uname -m)" = 'x86_64' ]; then
-  mv lib/x86_64 .
-  rm -r applemusic.apk lib/*
-  mv x86_64 lib/
+  unzip applemusic.apk "lib/x86_64/*"
 elif [ "$(uname -m)" = 'i686' ]; then
-  mv lib/x86 .
-  rm -r applemusic.apk lib/*
-  mv x86 lib/
+  unzip applemusic.apk "lib/x86/*"
 fi
 
 /opt/anisette_server
