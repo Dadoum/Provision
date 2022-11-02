1bff916c5f4cebe1aeffb81b75279cc1c38dbe94
Macley <26381427+Macleykun@users.noreply.github.com>
2022-11-02 21:49:13 +0100

    Fix if statement to run when there's no lib or when it's empty

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ ! -d "lib/" ]; then
+if [ -z "$(ls -A lib/ 2> /dev/null)" ]; then
   curl https://apps.mzstatic.com/content/android-apple-music-apk/applemusic.apk -O
   if [ "$(uname -m)" = 'aarch64' ]; then
     unzip applemusic.apk 'lib/arm64-v8a/libstoreservicescore.so' 'lib/arm64-v8a/libCoreADI.so'
