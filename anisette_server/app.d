86c7d0eb7e1f34cc2052e413e324ff68bf001a33
Dadoum <dadoum@protonmail.com>
2023-03-26 19:10:21 +0200

    Be more conservative about reprovisioning in anisette-server

--- anisette_server/app.d
+++ anisette_server/app.d
@@ -1,4 +1,5 @@
 import handy_httpd;
+import handy_httpd.components.request;
 import std.algorithm.searching;
 import std.array;
 import std.base64;
@@ -17,6 +18,7 @@ import constants;
 
 static __gshared ADI* adi;
 static __gshared ulong rinfo;
+static __gshared bool allowRemoteProvisioning = false;
 
 void main(string[] args) {
     auto serverConfig = ServerConfig.defaultValues;
@@ -35,6 +37,7 @@ void main(string[] args) {
         "a|adi-path", format!"Where the provisioning information should be stored on the computer (default: %s)"(path), &path,
         "init-only", format!"Download libraries and exit (default: %s)"(onlyInit), &onlyInit,
         "can-download", format!"If turned on, may download the dependencies automatically (default: %s)"(apkDownloadAllowed), &apkDownloadAllowed,
+        "allow-remote-reprovisioning", format!"If turned on, the server may reprovision the server on client demand (default: %s)"(allowRemoteProvisioning), &allowRemoteProvisioning,
     );
 
     if (helpInformation.helpWanted) {
@@ -102,57 +105,80 @@ void main(string[] args) {
 
     auto s = new HttpServer((ref ctx) {
         auto req = ctx.request;
-        auto res = ctx.response;
-        res.addHeader("Implementation-Version", anisetteServerBranding ~ " " ~ anisetteServerVersion);
+        ctx.response.addHeader("Implementation-Version", anisetteServerBranding ~ " " ~ anisetteServerVersion);
+
+        writeln("[<<] ", req.method, " ", req.url);
+        if (req.method != "GET") {
+            writefln("[>>] 405 Method Not Allowed");
+            ctx.response.setStatus(405).setStatusText("Method Not Allowed").flushHeaders();
+            return;
+        }
+
         if (req.url == "/reprovision") {
-            writeln("[<<] GET /reprovision");
-            adi.provisionDevice(rinfo);
-            writeln("[>>] 200 OK");
-            res.setStatus(200);
-        } else {
+            if (allowRemoteProvisioning) {
+                adi.provisionDevice(rinfo);
+                writeln("[>>] 200 OK");
+                ctx.response.setStatus(200);
+            } else {
+                writeln("[>>] 403 Forbidden");
+                ctx.response.setStatus(403).setStatusText("Forbidden").flushHeaders();
+            }
+            return;
+        }
+
+        if (req.url != "") {
+            writeln("[>>] 404 Not Found");
+            ctx.response.setStatus(404).setStatusText("Not Found").flushHeaders();
+            return;
+        }
+
+        try {
+            import std.datetime.systime;
+            import std.datetime.timezone;
+            import core.time;
+            auto time = Clock.currTime();
+
+            ubyte[] mid;
+            ubyte[] otp;
             try {
-                import std.datetime.systime;
-                import std.datetime.timezone;
-                import core.time;
-                auto time = Clock.currTime();
-
-                writefln("[<<] GET /");
-
-                ubyte[] mid;
-                ubyte[] otp;
-                try {
-                    adi.getOneTimePassword(mid, otp);
-                } catch (Throwable) {
-                    writeln("Reprovision needed.");
-                    adi.provisionDevice(rinfo);
-                    adi.getOneTimePassword(mid, otp);
+                adi.getOneTimePassword(mid, otp);
+            } catch (AnisetteException exception) {
+                if (exception.anisetteError() != AnisetteError.notProvisioned) {
+                    throw exception;
                 }
 
-                import std.conv;
-                import std.json;
-
-                JSONValue response = [
-                    "X-Apple-I-Client-Time": time.toISOExtString.split('.')[0] ~ "Z",
-                    "X-Apple-I-MD":  Base64.encode(otp),
-                    "X-Apple-I-MD-M": Base64.encode(mid),
-                    "X-Apple-I-MD-RINFO": to!string(rinfo),
-                    "X-Apple-I-MD-LU": adi.localUserUUID,
-                    "X-Apple-I-SRL-NO": adi.serialNo,
-                    "X-MMe-Client-Info": adi.clientInfo,
-                    "X-Apple-I-TimeZone": time.timezone.dstName,
-                    "X-Apple-Locale": "en_US",
-                    "X-Mme-Device-Id": adi.deviceId,
-                ];
-
-                writefln!"[>>] 200 OK %s"(response);
-
-                res.setStatus(200);
-                res.addHeader("Content-Type", "application/json");
-                res.writeBodyString(response.toString(JSONOptions.doNotEscapeSlashes));
-            } catch(Throwable t) {
-                res.setStatus(500);
-                res.writeBodyString(t.toString());
+                writeln("Machine wasn't provisioned??");
+                adi.provisionDevice(rinfo);
+                adi.getOneTimePassword(mid, otp); // if it rethrows an error, we can't do much anyway.
             }
+
+            import std.conv;
+            import std.json;
+
+            JSONValue response = [
+                "X-Apple-I-Client-Time": time.toISOExtString.split('.')[0] ~ "Z",
+                "X-Apple-I-MD":  Base64.encode(otp),
+                "X-Apple-I-MD-M": Base64.encode(mid),
+                "X-Apple-I-MD-RINFO": to!string(rinfo),
+                "X-Apple-I-MD-LU": adi.localUserUUID,
+                "X-Apple-I-SRL-NO": adi.serialNo,
+                "X-MMe-Client-Info": adi.clientInfo,
+                "X-Apple-I-TimeZone": time.timezone.dstName,
+                "X-Apple-Locale": "en_US",
+                "X-Mme-Device-Id": adi.deviceId,
+            ];
+            ctx.response.addHeader("Content-Type", "application/json");
+            ctx.response.writeBodyString(response.toString(JSONOptions.doNotEscapeSlashes));
+
+            writefln!"[>>] 200 OK %s"(response);
+            ctx.response.okResponse();
+        } catch(Throwable t) {
+            string exception = t.toString();
+            writeln("Encountered an error: ", exception);
+            writeln("[>>] 500 Internal Server Error");
+            ctx.response.writeBodyString(exception);
+
+            ctx.response.setStatus(500).setStatusText("Internal Server Error").flushHeaders();
         }
     }, serverConfig);
 
