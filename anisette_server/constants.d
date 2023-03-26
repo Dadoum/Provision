87f5074fee718ce1710051cf6782b149495ec9a6
Dadoum <dadoum@protonmail.com>
2023-03-26 19:19:11 +0200

    Enable actions on all branches

--- anisette_server/constants.d
+++ anisette_server/constants.d
@@ -1,5 +1,5 @@
 module constants;
 
 enum anisetteServerBranding = "anisette-server-provision";
-enum anisetteServerVersion = "1.2.1";
+enum anisetteServerVersion = "1.3.0";
 enum nativesUrl = "https://apps.mzstatic.com/content/android-apple-music-apk/applemusic.apk";
